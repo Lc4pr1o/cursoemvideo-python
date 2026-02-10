f08f2ab2c4bc74e937dc687d00cc3d4337bb0a8e
Leonardo Cardoso Malerbo <leonardo.cardosolc421@gmail.com>
2026-02-10 16:46:53 -0300

    uptade

--- exe5.py
+++ exe5.py
@@ -2,3 +2,8 @@
 # --- Crie um programa que leia um numeros inteiro e mostre na tela o seu sucessor e antecessor.
 
 
+pergunta = int(input('Digite um numero: '))
+ante = pergunta - 1
+suce = pergunta + 1
+
+print(f'O numero digitado: {pergunta} \n seu Sucessor é: {suce}\n seu Antecessor é: {ante}')
--- exe6.py
+++ exe6.py
@@ -1,2 +1,16 @@
 # --- DESAFIO 006
 # --- Crie um algoritmo que leia um numero e mostre o seu dorbo, Triplo e raiz quadrada.
+
+pergunta = int(input('Digite um numero: '))
+
+dobro = pergunta * 2
+triplo = pergunta * 3
+raiz = pergunta ** 0.5
+
+"""
+Raiz Quadrada = Elevar a 1/2 (ou 0.5)
+Raiz Cúbica = Elevar a 1/3 (ou 0.333)
+Raiz Quarta = Elevar a 1/4 (ou 0.25)
+"""
+
+print(f'O numero digitado foi: {pergunta} \n sendo que; \n Seu Dobro é: {dobro} \n Seu Triblo é: {triplo} \n Sua Raiz Quadrada é: {raiz:0.3}')
