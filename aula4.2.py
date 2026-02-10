d98ec4a2ddb935af780063f167d0b15d318e9a26
Leonardo Cardoso Malerbo <leonardo.cardosolc421@gmail.com>
2026-02-10 12:19:59 -0300

    uptade

--- aula4.2.py
+++ aula4.2.py
@@ -1,7 +1,6 @@
-n1 = float(input('Me passa o Primeiro numero que você quer somar? '))
-n2 = float(input('Me passa o Segundo numero que você quer somar? '))
+n1 = int(input('Me passa o Primeiro numero que você quer somar? '))
+n2 = int(input('Me passa o Segundo numero que você quer somar? '))
 
 soma = n1 + n2
 
-print('\n',
-      'A soma dos numeros são:', soma)
+print(f'\nA soma entre {n1} e {n2} \nresulta em: {soma}!')
--- exe2.py
+++ exe2.py
@@ -1,2 +1,2 @@
-nome = input('Qual o seu nome? ')
-print('Prazer em te conhecer '+ nome)
+nome = input('Digite seu nome: ')
+print('Prazer em te conhecer {}!'.format(nome))
