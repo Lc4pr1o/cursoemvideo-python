9107d639b74581195c2de7409fc4785525f293bf
Leonardo Cardoso Malerbo <leonardo.cardosolc421@gmail.com>
2026-02-16 21:57:13 -0300

    Update exe16.py

--- exe16.py
+++ exe16.py
@@ -1,5 +1,7 @@
 # --- Crie um programa que leia um numero real qualquer pelo teclado e mostre na tela a sua porção inteira.
 # --- DESAFIO 016
 
+import math
 num = (float(input('Digite um numero: ')))
 
+print(f'O número {num} tema parte inteira {math.trunc(num)}')
